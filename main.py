bb0210a370e912df76d6310fa2f8d0e000990749
Akashimoke <muhammadridhofathifauzan@gmail.com>
2026-05-22 08:13:27 +0700

    add Student discount

--- main.py
+++ main.py
@@ -3,4 +3,7 @@ def calculate_gym_membership(age: int, membership_type: str, access_day: str, me
         return "Rejected: Age below 12"
         
     base_rate = 50.0 
+    if membership_type == "Student":
+        base_rate *= 0.8
+        
     return base_rate * membership_duration
